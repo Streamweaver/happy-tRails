d3dbd151b15cc0389b8901104f5d018bf640944c
Streamweaver <Streamweaver@gmail.com>
2013-04-04 13:17:02 -0400

    Initial Rails App w/ Gems.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HappytrailsApp::Application.config.secret_token = '8294662331c6faceefe0ffb37dfccca6bfeba7fb7ba5ee8c4a9b80aef2bcd4198a42b8af16fc0efa08d8d84f260449c479b6d58a264414f5348cf1f33f702438'
