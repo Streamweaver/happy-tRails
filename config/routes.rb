f3ed308cf88f98c42d04c12372ca119ac6014a37
Streamweaver <Streamweaver@gmail.com>
2013-04-05 18:17:56 -0400

    Finished of Layout Exercises

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 HappytrailsApp::Application.routes.draw do
 
+  match '/signup',  to: 'users#new'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
