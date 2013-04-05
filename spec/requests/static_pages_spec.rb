7e2aca9b024fb1cd9afb209dc1d1dd718c084f52
Streamweaver <Streamweaver@gmail.com>
2013-04-05 17:58:15 -0400

    Broke out layout into partials.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,40 +5,51 @@ describe "StaticPages" do
   describe "Home Page" do
 
     it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+      visit root_path
+      page.should have_selector('h1', text: 'Sample App')
     end
 
     it "should have the title Home" do
-      visit '/static_pages/home' #static_pages_path_home
-      page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Home')
+      visit root_path
+      page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | Home')
     end
   end
 
   describe "Help Page" do
 
     it "should have title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Help')
+      visit help_path
+      page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | Help')
     end
 
     it "should have h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+      visit help_path
+      page.should have_selector('h1', text: 'Help')
     end
   end
 
   describe "About Page" do
 
     it "should have title 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About")
+      visit about_path
+      page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | About")
     end
 
     it "should have the h1 'About US'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+      visit about_path
+      page.should have_selector('h1', text: 'About Us')
     end
   end
 
+  describe "Contact Page" do
+    it "should have h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | Contact')
+    end
+  end
 end
