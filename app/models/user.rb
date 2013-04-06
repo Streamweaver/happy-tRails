3bc25048a66489ff927c9e73f96f019ac653c3e0
Streamweaver <Streamweaver@gmail.com>
2013-04-06 16:43:36 -0400

    Added validation to User email and index.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,13 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+
+  validates :name, presence: true, length: { maximum: 50 }
+
+  EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+
+  validates :email, presence: true, format: { with: EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,44 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = User.new(name: "Test User", email: "testuser@email.com") }
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  describe 'when name is not present' do
+    before { @user.name = '' }
+    it { should_not be_valid }
+  end
+
+  describe 'when name is too long' do
+    before { @user.name = 'a' * 51 }
+    it { should_not be_valid }
+  end
+
+  describe 'when email is not present' do
+    before { @user.email = '' }
+    it { should_not be_valid }
+  end
+
+  describe "when email format is not valid" do
+    it "should be invalid" do
+      invalid_list = ['this is not valid', "another_invalid_address", '@streamweaver']
+      invalid_list.each do |invalid_email|
+        @user.email = invalid_email
+        @user.should_not be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "Should be valid" do
+      valid_list = ['test@email.com', 'name.me@company.org', 'next.text@COMPANY.COM']
+      valid_list.each do |valid_email|
+        @user.email = valid_email
+        @user.should be_valid
+      end
+    end
+  end
 end
