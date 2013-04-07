ae7ce1028b1350b30cc965260e247a70adabbe44
Streamweaver <Streamweaver@gmail.com>
2013-04-06 23:38:22 -0400

    Completed the User Model w/ password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,17 @@
 #  updated_at :datetime         not null
 #
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
 
   before_save { |user| user.email = email.downcase}
 
   validates :name, presence: true, length: { maximum: 50 }
-
   EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
-
-  validates :email, presence: true, format: { with: EMAIL_REGEX },
+  validates :email,
+            presence: true,
+            format: { with: EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
