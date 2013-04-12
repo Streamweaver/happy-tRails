8a8ee8ad0292065937cb00b053ce8f11240624aa
Streamweaver <Streamweaver@gmail.com>
2013-04-12 16:34:30 -0400

    Finished user signup 7.4.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      flash[:success] = "User successfully created!"
       redirect_to @user
     else
       render 'new'
