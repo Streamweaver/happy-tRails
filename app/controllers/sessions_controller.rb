08b2802e112cba0740d85c84b11b18ec1d7c3091
Streamweaver <Streamweaver@gmail.com>
2013-04-13 21:44:03 -0400

    Signin Failures handled and texted.  Finished 8.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,17 @@
 class SessionsController < ApplicationController
 
-  def new
+  def create
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Sign In
+    else
+      # Redirect w/ errors
+      flash.now[:error] = "Invalid email or password.  Please try again."
+      render 'new'
+    end
   end
 
-  def create
+  def new
   end
 
   def destroy
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -16,10 +16,16 @@ describe "AuthenticationPages" do
     before { visit signin_path }
 
     describe "with invalid login information" do
-      before { click_butto "Sign in" }
+      before { click_button "Sign in" }
 
       it { should have_selector('title', text: 'Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home"}
+
+        it { should_not have_selector('div.alert.alert-error') }
+      end
 
     end
 
