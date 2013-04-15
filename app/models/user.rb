98e82f0a86232bb8706338a63deb840d913a2a2f
Streamweaver <Streamweaver@gmail.com>
2013-04-14 20:54:33 -0400

    Working SignIn and Remember token.  Finishes 8.2.4.  Bootstrap menus not working

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { |user| user.email = email.downcase}
+  before_save :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
@@ -24,4 +25,10 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  private
+
+  def create_remember_token
+     self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,8 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   describe 'when name is not present' do
     before { @user.name = '' }
@@ -96,5 +98,10 @@ describe User do
       it { should be_invalid }
     end
 
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
