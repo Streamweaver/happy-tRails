d1e98cade4467f0dfea11a6ee5776e8d3984d484
Streamweaver <Streamweaver@gmail.com>
2013-04-15 17:40:17 -0400

    Added Basic User Updates.Finished 9.1.2

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe "User Pages" do
 
   subject { page }
 
@@ -54,6 +54,25 @@ describe "UserPages" do
 
     it { should have_selector('h1', text: user.name) }
     it { should have_selector('title', text: user.name) }
+  end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user)}
+
+    before { visit edit_user_path(user)}
+
+    describe "page" do
+      it { should have_selector('h1',    text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
   end
 
 end
