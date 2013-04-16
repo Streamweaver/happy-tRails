16442a592439dca8d47f63c6843e7e355227191e
Streamweaver <Streamweaver@gmail.com>
2013-04-16 14:50:10 -0400

    Editing user info works.  Finished 9.1.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
-      flash[:success] = "User updated successfully."
+      flash[:success] = "Profile Updated."
+      sign_in @user
       redirect_to @user
     else
       render 'edit'
